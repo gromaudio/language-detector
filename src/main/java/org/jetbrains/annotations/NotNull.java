4f4f9644c7ecf2eb59c26b19ad1c9e862f23c677
nicolausyes <nicolausyes@gmail.com>
2017-10-13 12:23:22 +0300

    Remove jetbrains annotation dependency.

---
+++ src/main/java/org/jetbrains/annotations/NotNull.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2000-2012 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.jetbrains.annotations;
+
+import java.lang.annotation.*;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PACKAGE;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.RetentionPolicy.CLASS;
+
+/**
+ * An element annotated with NotNull claims {@code null} value is <em>forbidden</em>
+ * to return (for methods), pass to (parameters) and hold (local variables and fields).
+ * Apart from documentation purposes this annotation is intended to be used by static analysis tools
+ * to validate against probable runtime errors and element contract violations.
+ *
+ * @author max
+ */
+@Documented
+@Retention(CLASS)
+@Target({METHOD, PARAMETER, FIELD, ANNOTATION_TYPE, PACKAGE})
+public @interface NotNull {
+}
